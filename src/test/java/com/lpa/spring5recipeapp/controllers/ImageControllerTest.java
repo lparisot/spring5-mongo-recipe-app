7d11f63f2350df2f2ecfb5fc01907e508f6060c3
L.P <parisot.laurent@gmail.com>
2017-10-10 12:55:36 +0200

    Repairing integration test for MongoDB

--- src/test/java/com/lpa/spring5recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/com/lpa/spring5recipeapp/controllers/ImageControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
